98ee99c73d4c15d43bcc7ef99a6bf488cb70027e
Krish Agrawal <63946679+krish-ag@users.noreply.github.com>
2024-01-13 20:30:00 +0530

    Time: 3 ms (86.03%) | Memory: 45.1 MB (29.06%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -3,20 +3,20 @@ class Solution {
         if (s.length() < 2 || numRows < 2) return s;
 
         int next = 2 * numRows - 2;
-        String st = "";
+        StringBuilder st = new StringBuilder();
 
         for (int i = 0; i < numRows; i++) {
             int x = i;
             while (x < s.length()){
-                st += s.charAt(x);
+                st.append(s.charAt(x));
                 if ((x+next - 2*i) != x && (x+next - 2*i) != x+next && (x+next -2*i) < s.length()){
-                    st += s.charAt(x+next - 2*i);
+                    st.append(s.charAt(x+next - 2*i));
                 }
                 x += next;
 
             }
         }
-        return st;
+        return st.toString();
         
     }
 }
