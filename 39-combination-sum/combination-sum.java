f52702e02b43604f07e097a1b2225a6bee896c90
Krish Agrawal <63946679+krish-ag@users.noreply.github.com>
2024-01-31 12:14:54 +0530

    Time: 2 ms (84.18%) | Memory: 44.9 MB (16.97%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<Integer> e = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        getCoinsSum(candidates, target, e, 0, result);
+        return result;
+    }
+
+    public static int getCoinsSum(int[] coins, int sum, List<Integer> ll, int i, List<List<Integer>> ans) {
+        if (sum == 0) {
+            ans.add(ll);
+            return 1;
+        }
+
+        int res = 0;
+        for (; i < coins.length; i++) {
+            if (sum < coins[i]) {
+                continue;
+            }
+//            if (!used[i]) {
+//                used[i] = true;
+            ll.add(coins[i]);
+            res += getCoinsSum(coins, sum - coins[i], new ArrayList<>(ll), i, ans);
+            ll.remove(ll.size() - 1);
+//                used[i] = false;
+//        }
+        }
+        return res;
+    }
+
+
+}
