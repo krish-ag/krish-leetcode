9fe7f80d6c2f38538b998d8f3519a0284622c430
Krish Agrawal <63946679+krish-ag@users.noreply.github.com>
2024-03-15 23:18:58 +0530

    Time: 64 ms (8.78%) | Memory: 55.4 MB (5.84%) - LeetSync

---
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    if (nums.length === 0) return init;
+    return reduce(nums.slice(1), fn, fn(init, nums[0]))
+};
