49ec1b2577e4dade5242271ecbddaab623397209
Krish Agrawal <63946679+krish-ag@users.noreply.github.com>
2024-07-07 23:23:04 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (38.70%) - LeetSync

--- 1642-water-bottles/water-bottles.java
+++ 1642-water-bottles/water-bottles.java
@@ -1,14 +1,6 @@
 class Solution {
     public int numWaterBottles(int nB, int nE) {
-        int count = 0;
-        int left = 0;
-        while (nB > 0) {
-            count += nB;
-            int temp = left;
-            left = (nB+left) % nE;
-            nB = (nB + temp) / nE;
-        }
-        return count;
+        return nB + (nB-1) / (nE-1);
 
         
     }
